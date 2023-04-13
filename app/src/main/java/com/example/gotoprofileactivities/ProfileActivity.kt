8d15a05ec3954da3dc5a59326f356896d7cfcc32
unknown <monaspa@tutanota.com>
2023-04-14 00:42:08 +0400

    initial commit

---
+++ app/src/main/java/com/example/gotoprofileactivities/ProfileActivity.kt
@@ -0,0 +1,18 @@
+package com.example.gotoprofileactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        }
+    }
+}
